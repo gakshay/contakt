3e39e5d58d20e977e6caa1e0dc33277adc5c21fd
Akshay Gupta <akshay.aries11@gmail.com>
2013-01-14 23:52:21 +0530

    added forums and voicemail icons

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,6 +7,7 @@ class Ability
     can :update, Address, :user_id => user.id
     can :destroy, Address, :user_id => user.id
     
+    
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
