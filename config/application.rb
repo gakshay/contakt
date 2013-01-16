24c476aeb54bfffcdc1bd9580c99a7affc30849a
Akshay Gupta <akshay.aries11@gmail.com>
2013-01-16 10:03:08 +0530

    remote asset_sync

--- config/application.rb
+++ config/application.rb
@@ -64,7 +64,7 @@ module ContaktDemo
     
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    config.action_mailer.default_url_options = { :host => 'edakia.in' }
+    config.action_mailer.default_url_options = { :host => 'contaktus.com' }
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.perform_deliveries = true
     config.action_mailer.raise_delivery_errors = false
