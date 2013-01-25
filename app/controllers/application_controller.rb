5ad61bd05e6723bf36ece7dd44ad0183cdae67f1
Akshay Gupta <akshay.aries11@gmail.com>
2013-01-25 20:26:36 +0530

    changes in register, login and header and footer UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  layout :layout
 
-  private
-
-  def layout
-      # only turn it off for login and registration pages:
-    if is_a?(Devise::RegistrationsController) or is_a?(Devise::SessionsController)
-      "plain"
-    else
-      "application"  
-    end
-  end
-  
-  
 end
